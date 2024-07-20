a8bfcc09f963fdb0b2bd1254218d8da8b1a3c5e2
7321kartik <7321kartikgound@gmail.com>
2024-07-20 12:36:36 +0530

    Initilize Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import LoginForm from './componenets/Login.jsx'
+
+function App() {
+  return <>
+    <LoginForm />
+  </>
+}
+
+export default App
---
+++ src/componenets/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError('');
+
+    try {
+      const response = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      console.log(data)
+
+      if (!data.true) {
+        alert(data.message);
+        setShowMessage(true);
+        return false;
+      }
+      console.log('Login successful:', data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-full max-w-sm">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h3>{showMessage && "check your Credentials"}</h3>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
